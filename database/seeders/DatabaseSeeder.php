4b9e78496f113fc3aa0432c856905a0d652091da
nabilamani <nabilamani2304@gmail.com>
2025-10-29 21:11:44 +0700

    show event modul

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,6 +22,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DetailPesertaSeeder::class,
             DetailAdminSeeder::class,
+            ModulAcaraSeeder::class,
         ]);
     }
 }
