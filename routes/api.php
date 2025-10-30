2d452fece9bcb4b97b23fb43229561c029e191ec
Syah Rizan Nazri Muhammad <snazrimuh@gmail.com>
2025-10-30 09:37:19 +0700

    add dashboard admin

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ModulAcaraController;
+use App\Http\Controllers\DashboardAdminController;
 
 // Public routes
 Route::post('/register', [AuthController::class, 'register']);
@@ -22,5 +23,6 @@ Route::middleware('auth:sanctum')->group(function () {
     });
     
     Route::get('/events', [ModulAcaraController::class, 'index']);
+    Route::get('/dashboard-admin/stats', [DashboardAdminController::class, 'stats']);
 
 });
