bfcbe95fc10f48adfeda7c554f2d5de2257e9b3c
yohanestedy <yohanestedy52b@gmail.com>
2025-11-02 08:18:03 +0700

    fix on presensi toggle

--- app/Http/Controllers/ToggleQRController.php
+++ app/Http/Controllers/ToggleQRController.php
@@ -24,6 +24,9 @@ class ToggleQRController extends Controller
 
         $event = ModulAcara::findOrFail($id);
 
+        // Konversi boolean ke integer
+        $status = $request->boolean('status_qr') ? 1 : 0;
+
         // Update status presensi
         $event->update([
             'mdl_presensi_aktif' => $request->status_qr,
