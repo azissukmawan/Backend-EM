228fe2cfe9bd8c48fe41210003ef228acfbd264f
fakhri <fakhrizon607@gmail.com>
2025-11-14 15:47:08 +0700

    fix bug listsaya (mobile)

--- routes/api.php
+++ routes/api.php
@@ -136,7 +136,7 @@ Route::middleware(['auth:sanctum', 'peserta'])->group(function () {
     Route::delete('/events/{eventId}/batal-daftar', [PendaftaranAcaraController::class, 'batalDaftar']);
     Route::get('/me/pendaftaran', [PendaftaranAcaraController::class, 'listSayaV2']);
     // Route::get('/me/pendaftaran-V2', [PendaftaranAcaraController::class, 'listSayaV2']);
-    Route::get('/me/pendaftaran-mobile', [PendaftaranAcaraController::class, 'listSayaMobile']);
+    Route::get('/me/pendaftaran-mobile', [PendaftaranAcaraController::class, 'listSayaMobileV2']);
     Route::get('events/{eventId}/me', [PendaftaranAcaraController::class, 'detailEventSaya']);
     Route::post('/events/{eventid}/generate-sertif', [GenerateSertifikatController::class, 'generateNomorSertifikat']);
 
